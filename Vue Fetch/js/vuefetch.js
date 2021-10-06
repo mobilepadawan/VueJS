82a727bd5463b92d5c56a15f6f995840322b16a0
Fernando O <fernando@vidamobile.com.ar>
2021-10-06 18:03:58 -0300

    Update vuefetch.js

--- Vue Fetch/js/vuefetch.js
+++ Vue Fetch/js/vuefetch.js
@@ -6,14 +6,13 @@ const app = new Vue(
         },
         methods: {
             async getCursos() {
-                console.time()
                 await fetch("https://615ceedec29813001773636d.mockapi.io/cursos")
                         .then(response => response.json())
                         .then(data => {this.cursos = data})
                         .then(data => {console.table(this.cursos)})
                         .catch(err => console.error(`${err}`))
                         .finally(() => {
-                            console.timeEnd()
+                            console.log("Finalizó la petición de datos remotos...")
                         })
             }
         }
