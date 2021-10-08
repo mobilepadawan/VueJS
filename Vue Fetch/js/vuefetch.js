4dc424774679f0d2529c87072f3aa9a06caa68a0
Fernando O <fernando@vidamobile.com.ar>
2021-10-08 16:09:05 -0300

    Update vuefetch.js

--- Vue Fetch/js/vuefetch.js
+++ Vue Fetch/js/vuefetch.js
@@ -1,12 +1,15 @@
+const axios = require('axios')
+
 const app = new Vue(
     {
         el: "#app",
         data: {
+            URL: "https://615ceedec29813001773636d.mockapi.io/cursos",
             cursos: []
         },
         methods: {
             async getCursos() {
-                await fetch("https://615ceedec29813001773636d.mockapi.io/cursos")
+                await fetch(this.URL)
                         .then(response => response.json())
                         .then(data => {this.cursos = data})
                         .then(data => {console.table(this.cursos)})
@@ -15,22 +18,24 @@ const app = new Vue(
                             console.log("Finalizó la petición de datos remotos...")
                         })
             },
-async postCurso() {
-    const cursoData = {
-        "nombre": "Typescript",
-        "creado": "2021-10-07",
-        "duracion": "20"
+            async postCurso() {
+                const cursoData = {
+                    "nombre": "Typescript",
+                    "creado": "2021-10-07",
+                    "duracion": "20"
+                }
+                const encabezado = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json; charset=UTF-8",
+                    },
+                    body: JSON.stringify(this.cursoData)
+                }
+                await fetch(this.URL, encabezado)
+                        .then((response) => response.json())
+                        .then((json) => console.log(json))
+            }
+
+        }
     }
-    const encabezado = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8",
-        },
-        body: JSON.stringify(this.cursoData)
-    }
-    await fetch("https://615ceedec29813001773636d.mockapi.io/cursos", encabezado)
-            .then((response) => response.json())
-            .then((json) => console.log(json))
-        } 
-    }
-})
+)
