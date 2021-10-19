6721872c27b570eb42d92334f53f562fecb1265a
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-10-19 13:31:30 -0300

    Improving the HTML part of the exercise

--- Web Components/js/app.js
+++ Web Components/js/app.js
@@ -1,14 +1,16 @@
-Vue.component('trailerflix-card', {
+Vue.component('codermeals-card', {
     props: {
-        cover: String,
-        title: String
-    },
+            title: String,
+            cover: String,
+            costo: Number
+        },
     template: `<div align="center">
                     <div>
-                        <img :src="cover" :title="title">
+                        <img class="img-thumbnail img-cropped" :src="cover" :title="title">
                         <br><br>
                         <p>{{ title }}</p>
-                        <br>
+                        <p class="text-black">$<strong> {{ costo }}</strong></p>
+                        <!-- <button class="btn btn-success btn-rounded">Agregar</button> --> 
                     </div>
                 </div>`,
 })
@@ -16,25 +18,35 @@ Vue.component('trailerflix-card', {
 var app = new Vue({
     el: '#app',
     data: {
-        titulo: "The Social Network",
-        portada: "https://upload.wikimedia.org/wikipedia/en/8/8c/The_Social_Network_film_poster.png",
-        coderflix: [
-                    {
-                     id: 1, 
-                     titulo: "The Social Network",
-                     portada: "https://upload.wikimedia.org/wikipedia/en/8/8c/The_Social_Network_film_poster.png"
-                    },
-                    {
-                     id: 2, 
-                     titulo: "The Circle",
-                     portada: "https://upload.wikimedia.org/wikipedia/en/8/80/The_Circle_%282017_film%29.png"
-                    },
-                    {
-                     id: 3, 
-                     titulo: "Antitrust", 
-                     portada: "https://upload.wikimedia.org/wikipedia/en/2/2a/Antitrust_poster.jpg"
-                    }
-                   ]
+        titulo: "Pizza Napoletana ai carciofi", 
+        portada: "https://static.cookist.it/wp-content/uploads/sites/21/2017/12/istock-480277738.jpg",
+        costo: 675.00,
+        codermeals: [
+            {
+             id: 1, 
+             titulo: "Spaghetti alla puttanesca",
+             costo: 575.00,
+             portada: "https://4.bp.blogspot.com/-D5Wvi_gX_Kg/WLatk_GVnKI/AAAAAAAAA5M/-y0gB26R0Dkb01QmdgfZqQALtd9NMV2DACLcB/s1600/P70301-072354.jpg"
+            },
+            {
+             id: 2, 
+             titulo: "Pizza Napoletana ai carciofi",
+             costo: 675.00,
+             portada: "https://static.cookist.it/wp-content/uploads/sites/21/2017/12/istock-480277738.jpg"
+            },
+            {
+             id: 3, 
+             titulo: "Porchetta umbra a cottura lunga", 
+             costo: 845.00,
+             portada: "https://www.fontecesia.it/wp-content/uploads/2018/11/porchetta-umbra-1920x1280.jpg"
+            },
+            {   
+            id: 4, 
+            titulo: "Orecchiette alle cime di rapa", 
+            costo: 845.00,
+            portada: "https://irepo.primecp.com/2016/03/259860/recipe-8673_ExtraLarge1000_ID-1461628.jpg"
+            }
+           ]
     },
     methods: {
 
