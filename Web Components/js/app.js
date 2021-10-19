6e30ec4e14a189e881ea222930429768695969ea
Fernando O <fernando@vidamobile.com.ar>
2021-10-19 09:04:51 -0300

    Closing time of creation

--- Web Components/js/app.js
+++ Web Components/js/app.js
@@ -1,5 +1,4 @@
 Vue.component('trailerflix-card', {
-    //props: ['cover', 'title'],
     props: {
         cover: String,
         title: String
@@ -20,9 +19,21 @@ var app = new Vue({
         titulo: "The Social Network",
         portada: "https://upload.wikimedia.org/wikipedia/en/8/8c/The_Social_Network_film_poster.png",
         coderflix: [
-                    {id: 1, titulo: "The Social Network", portada: "https://upload.wikimedia.org/wikipedia/en/8/8c/The_Social_Network_film_poster.png"},
-                    {id: 2, titulo: "The Circle", portada: "https://upload.wikimedia.org/wikipedia/en/8/80/The_Circle_%282017_film%29.png"},
-                    {id: 3, titulo: "Antitrust", portada: "https://upload.wikimedia.org/wikipedia/en/2/2a/Antitrust_poster.jpg"}
+                    {
+                     id: 1, 
+                     titulo: "The Social Network",
+                     portada: "https://upload.wikimedia.org/wikipedia/en/8/8c/The_Social_Network_film_poster.png"
+                    },
+                    {
+                     id: 2, 
+                     titulo: "The Circle",
+                     portada: "https://upload.wikimedia.org/wikipedia/en/8/80/The_Circle_%282017_film%29.png"
+                    },
+                    {
+                     id: 3, 
+                     titulo: "Antitrust", 
+                     portada: "https://upload.wikimedia.org/wikipedia/en/2/2a/Antitrust_poster.jpg"
+                    }
                    ]
     },
     methods: {
