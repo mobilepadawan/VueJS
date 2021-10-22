e701806a9abc44428966e10d84b9c1d93f63cb07
Fernando O <fernando@vidamobile.com.ar>
2021-10-22 08:20:55 -0300

    Showing and editing a web component file.

--- mi-coder-app/src/App.vue
+++ mi-coder-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="CH logo" src="./assets/logo.png" width="160px">
     <HelloWorld msg="Bienvenidos a CODERHOUSE."/>
   </div>
 </template>
