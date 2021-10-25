b33c2627925ccff66fc1e25cb6340acbb85558b3
Fernando O <fernando@vidamobile.com.ar>
2021-10-24 21:47:03 -0300

    Adding watchers

--- mi-coder-app/src/components/HelloWorld.vue
+++ mi-coder-app/src/components/HelloWorld.vue
@@ -8,8 +8,37 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    error: Number,
+		mensaje: String,
+		icono: String,
+		fechaError: Date
+  },
+	data: {
+		question: '',
+    answer: 'Has una pregunta para obtener tu respuesta!'
+	},
+	watch: {
+    question: function (newQuestion, oldQuestion) {
+      this.answer = 'Esperando que deje de escribir...'
+      this.debouncedGetAnswer()
+    },
+		methods: {
+			getAnswer:  function () {
+				if (this.question.indexOf('?') === -1) {
+					this.answer = 'Las preguntas contienen signo de interrogación. ;-)'
+					return
+				}
+				this.answer = 'Pensando...'
+				var vm = this
+				axios.get('https://yesno.wtf/api')
+					.then(function (response) {
+						vm.answer = _.capitalize(response.data.answer)
+					})
+					.catch(function (error) {
+						vm.answer = `¡Error! La API no respondió: ${error}`
+					})
+			}
+		}
 }
 </script>
 
