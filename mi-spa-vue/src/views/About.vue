202f7d03c9ba11a2e754fe78449b20eb74d52600
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-11-03 08:30:46 -0300

    My first SPA with Vue CLI

---
+++ mi-spa-vue/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Acerca de...</h1>
+    <p>Página dedicada al apartado Acerca de...</p>
+  </div>
+</template>
---
+++ mi-spa-vue/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" class="img-logo" src="../assets/logo.jpg">
+    <HelloWorld msg="Bienvenido a tu SPA"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+  .img-logo {
+    width: 6em;
+  }
+</style>
