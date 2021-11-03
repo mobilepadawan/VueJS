859a2a1a0647917b376cfd289e4969fc81e985ef
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-11-03 18:35:38 -0300

    Closing samples

--- mi-spa-vue/src/views/About.vue
+++ mi-spa-vue/src/views/About.vue
@@ -4,3 +4,23 @@
     <p>Página dedicada al apartado Acerca de...</p>
   </div>
 </template>
+
+<script>
+export default {
+    data: {
+      nombreAplicacion: 'Mi App Vue/Cli'
+    },
+    beforeCreate: ()=> {
+      console.log('En este momento del ciclo, data: no existe aún.')
+    },
+    created: ()=> {
+      console.log('En este momento del ciclo, ya puedo acceder a data: ' + $this.data.nombreAplicacion)
+    },
+    mounted: ()=> {
+          this.$nextTick() => {
+            console.log(this.$el.querySelector('h1'))
+          }
+    }
+  }
+}
+</script>
