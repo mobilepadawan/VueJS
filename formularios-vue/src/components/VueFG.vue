eda87f2d3c154db7c18c55b66feaf3473351ef4c
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-11-05 18:26:19 -0300

    Adding script part

--- formularios-vue/src/components/VueFG.vue
+++ formularios-vue/src/components/VueFG.vue
@@ -1,37 +1,42 @@
 <template> 
  <div id="app">
-  <vue-form :state="formstate" @submit.prevent="onSubmit">
- 
+    <vue-form :state="formstate" @submit.prevent="onSubmit">
     <validate tag="label">
-      <span>Name *</span>
-      <input v-model="model.name" required name="name" />
- 
-      <field-messages name="name">
-        <div>Success!</div>
-        <div slot="required">Name is a required field</div>
-      </field-messages>
+        <span>Nombre *</span>
+        <input v-model="model.name" required name="name" />
+        <field-messages name="name">
+        <div>Correcto!</div>
+        <div slot="required">Nombre es un campo requerido</div>
+        </field-messages>
     </validate>
- 
     <validate tag="label">
-      <span>Email</span>
-      <input v-model="model.email" name="email" type="email" required />
- 
-      <field-messages name="email">
-        <div slot="required">Email is a required field</div>
-        <div slot="email">Email is not valid</div>
-      </field-messages>
+        <span>Email</span>
+        <input v-model="model.email" name="email" type="email" required />
+        <field-messages name="email">
+        <div slot="required">Email es un campo requerido</div>
+        <div slot="email">Email no es válido</div>
+        </field-messages>
     </validate>
- 
-    <button type="submit">Submit</button>
-  </vue-form>
-  <pre>{{ formstate }}</pre>
+    <button type="submit">ENVIAR</button>
+    </vue-form>
+    <pre>{{ formstate }}</pre>
 </div>
 </template>
 
 <script>
 export default {
   name: 'VueFG',
-
+  data: {
+    formstate: {},
+    model: {
+      name: '',
+      email: '',
+      phone: '',
+      department: null,
+      comments: '',
+      notValidated: '',
+      agree: false
+    }
 }
 </script>
 
