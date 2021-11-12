37f1fa82a1750f7bfbc1eb76194e5da068addffe
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-11-12 11:17:26 -0300

    Updating index.js

--- vuex-app/src/store/index.js
+++ vuex-app/src/store/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+<<<<<<< Updated upstream
 import sucursales from '/src/modules/sucursales'
 
+=======
+//store/index.js
+>>>>>>> Stashed changes
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  namespaced: true,
   state: {
     msg: 'Variable de State declarada en Store.',
     nombreDelCurso: 'Vue y Vuex!',
