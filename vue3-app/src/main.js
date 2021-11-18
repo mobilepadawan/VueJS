b8a5f478588b92fc44a4f6df1b936dd870a7605d
Fernando O <fernando@vidamobile.com.ar>
2021-11-18 13:15:44 -0300

    Adding the first Vue3 project. Just for Education purposes

---
+++ vue3-app/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, onActivated, onDeactivated, onErrorCaptured } from 'vue'
+
+createApp(App).mount('#app')
+
